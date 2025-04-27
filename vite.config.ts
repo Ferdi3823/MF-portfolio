d964ac4e94d837ffeb50ae44f338a3bfbf52a22a
Ferdinand Monnan <ferdi3823@gmail.com>
2025-04-27 14:02:59 +0200

    nom domaine 2

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/MF-portfolio/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
